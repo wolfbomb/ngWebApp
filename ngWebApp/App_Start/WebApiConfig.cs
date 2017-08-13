482aa51bd6d5cfa151981e8f504dfb10094e9752
wolfbomb <nimrod.resulta@gmail.com>
2017-08-13 23:13:47 +0800

    Initial commit

---
+++ ngWebApp/App_Start/WebApiConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ngWebApp
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration configuration)
+        {
+            configuration.Routes.MapHttpRoute("API Default", "api/{controller}/{id}",
+              new { id = RouteParameter.Optional });
+        }
+    }
+}
---
+++ ngWebApp/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using ngWebApp.App_Start;
+using System.Web.Http;
+
+namespace ngWebApp
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
